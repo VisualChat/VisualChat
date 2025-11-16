d7dbdc6555cf508568dc5841b71ba342ee4fd3c8
Sravani Pagidela <sravani2201@outlook.com>
2025-11-15 18:23:30 -0800

    Initial Commit

---
+++ VisualChat/VisualChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisualChatApp.swift
+//  VisualChat
+//
+//  Created by Srinivasa Chaitanya Gopireddy on 11/15/25.
+//
+
+import SwiftUI
+
+@main
+struct VisualChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
